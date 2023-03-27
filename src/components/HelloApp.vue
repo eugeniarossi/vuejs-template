d7e8d48159d3a1f26b14a229b75689bd5263fdfe
eugeniarossi <eugenia.rossi@outlook.com>
2023-03-27 16:09:17 +0200

    feat: scss installed

---
+++ src/components/HelloApp.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+    name: 'HelloApp'
+}
+</script>
+
+<template>
+    <h2>Hello</h2>
+</template>
+
+<style scoped lang="scss">
+    h2 {
+        color: blue;
+    }
+</style>
